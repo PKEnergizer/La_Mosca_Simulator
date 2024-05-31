7ccd984828f5faf52756216882958c22b5747245
pkenergizer <pktenshi@gmail.com>
2024-05-31 16:55:35 +0300

    v7.1

---
+++ Assets/Scritpts/SoundManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumenSlider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+             PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }else{
+            Load();
+        }
+       
+    }
+
+    public void ChangeVolumen()
+    {
+        AudioListener.volume = volumenSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumenSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumenSlider.value);
+    }
+}
